e0afd84e38cb2d7830693f160171b64325008de2
lafrefre <134004859+lafrefre@users.noreply.github.com>
2023-07-14 15:30:00 -0700

    "Added my javascript questions"

---
+++ script.js
@@ -0,0 +1,113 @@
+let questions = [
+    {
+    numb: 1,
+    question: "How many toes are on each foot?",
+    answer: "5",
+    options: [
+        "11",
+        "14",
+        "5",
+        "1"
+    ]
+},
+{
+    numb: 2,
+    question: "Are toes ediable?",
+    answer: "No",
+    options: [
+        "No",
+        "Yes",
+        "On occasion",
+        "They are suckable"
+    ]
+},
+{
+    numb: 3,
+    question: "How many toes are on both feet?",
+    answer: "10",
+    options: [
+        "69",
+        "10",
+        "5",
+        "None"
+    ]
+},
+{
+    numb: 4,
+    question: "How many joints are in the foot",
+    answer: "33",
+    options: [
+        "45",
+        "23",
+        "33",
+        "15"
+    ]
+},
+{
+    numb: 5,
+    question: "How much percentage of your bones are in your feet?",
+    answer: "25%",
+    options: [
+        "13%",
+        "50%",
+        "25%",
+        "26%"
+    ]
+},
+{
+    numb: 6,
+    question: "How long does it take for you to grow a brand new toe-nail?",
+    answer: "5-6 months",
+    options: [
+        "5-6 months",
+        "3-4 months",
+        "3-4 weeks",
+        "1 year"
+    ]
+},
+{
+    numb: 7,
+    question: "Do animales has toes?",
+    answer: "yes",
+    options: [
+        "Yes",
+        "No",
+    ]
+},
+{
+    numb: 8,
+    question: "Why do feet stink?",
+    answer: "Bacteria",
+    options: [
+        "Left over mayo left in shoes",
+        "Bacteria",
+        "No feet",
+        "Lack of hygiene"
+    ]
+},
+{
+    numb: 9,
+    question: "What is the record for longest big toe?",
+    answer: "5inches",
+    options: [
+        "12inches",
+        "3inches",
+        "5inches",
+        "10 inches"
+    ]
+},
+{
+    numb: 10,
+    question: "What is a commen foot condition?",
+    answer: "Bunions",
+    options: [
+        "Onions",
+        "Bunions",
+        "Menopause",
+        "Satellite Tv"
+    ]
+},
+
+
+
+]
